19e15f2f3efe8203995482cbd61568e3a500ca46
Svenden123 <buhrus1@yandex.ru>
2021-08-12 21:11:54 +0400

    first commit

---
+++ main.py
@@ -0,0 +1,52 @@
+#x = 0
+
+#while x < 5:
+    #print(f'Текущее значение x равно {x}')
+    #x = x + 1
+
+
+#y = 0
+
+#while y < 5:
+    #print(f'Текущее значение y равно {y}')
+
+
+
+
+#x = 0
+
+#while x < 5:
+    #print(f'Текущее значение x равно {x}')
+    #x += 1
+
+#else:
+    #print('x больше или равен 5')
+
+#mystring1 = 'Hello World!'
+#for letter1 in mystring1:
+    #print(letter1)
+
+
+
+#mystring = "Sammy"
+#for letter in mystring:
+    #if letter == 'a':
+        #continue
+    #print(letter)
+
+
+#mystring = "Sammy"
+#for letter in mystring:
+    #if letter == 'a':
+        #break
+    #print(letter)
+
+
+x = 0
+
+while x < 5:
+    if x == 2:
+        break
+    print(x)
+    x+=1
+
